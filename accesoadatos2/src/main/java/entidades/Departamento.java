990e9e91c043aed0395cdc58a8fa168d396e1258
luisdbb <luisdbb@educastur.org>
2024-10-21 18:35:36 +0200

    modificada Departamento.java nuevo campo int capacidad

--- accesoadatos2/src/main/java/entidades/Departamento.java
+++ accesoadatos2/src/main/java/entidades/Departamento.java
@@ -4,31 +4,47 @@ public class Departamento {
 
 	private Long id;
 	private String nombre;
-	
-	
-	public Departamento() {}
-	
-	public Departamento(Long id, String nombre) {
+	private int capacidad;
+
+	public Departamento() {
+	}
+
+	public Departamento(Long id, String nombre, int capacidad) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
+		this.capacidad = capacidad;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
 	@Override
 	public String toString() {
-		return "Departamento [id=" + id + ", nombre=" + nombre + "]";
+		return "Departamento [id=" + id + ", nombre=" + nombre + ", capacidad=" + capacidad + "]";
 	}
 	
 	
+
 }
