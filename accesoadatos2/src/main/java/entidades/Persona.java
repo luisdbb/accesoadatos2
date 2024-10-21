ff25fb81785bd932112bda356b1efa87ae90e87b
luis <luis@EQLBB2024.home>
2024-10-21 18:28:14 +0200

    añadida entidades.Departamento actualizada entidades.Persona

--- accesoadatos2/src/main/java/entidades/Persona.java
+++ accesoadatos2/src/main/java/entidades/Persona.java
@@ -1,7 +1,66 @@
 package entidades;
 
+import java.util.Objects;
+
 public class Persona {
-  private Long id;
-  private String nombre;
-  private int edad;
+	private Long id;
+	private String nombre;
+	private int edad;
+
+	public Persona() {
+	}
+
+	public Persona(Long id, String nombre, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(edad, id, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return edad == other.edad && Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", nombre=" + nombre + ", edad=" + edad + "]";
+	}
+
 }
